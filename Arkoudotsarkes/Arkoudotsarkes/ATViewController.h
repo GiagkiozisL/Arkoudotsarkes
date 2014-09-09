36e498e3763df0b550647319e06a6a0e06a9a726
GiagkiozisL <giagkiozis.l@hotmail.com>
2014-09-10 01:10:25 +0300

    init app

---
+++ Arkoudotsarkes/Arkoudotsarkes/ATViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATViewController.h
+//  Arkoudotsarkes
+//
+//  Created by Giagkiozis Louloudis on 9/10/14.
+//  Copyright (c) 2014 FlowerApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATViewController : UIViewController
+
+@end
---
+++ Arkoudotsarkes/Arkoudotsarkes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Arkoudotsarkes
+//
+//  Created by Giagkiozis Louloudis on 9/10/14.
+//  Copyright (c) 2014 FlowerApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ATAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ATAppDelegate class]));
+    }
+}
